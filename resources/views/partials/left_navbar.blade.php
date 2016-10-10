b80d9e3fa73b643c5c2350b5ff573867d1e2a6a2
Miguel Romero <mromero@unnaki.com.py>
2016-10-10 15:27:00 -0300

    First Commit - New Skin added

---
+++ resources/views/partials/left_navbar.blade.php
@@ -0,0 +1,80 @@
+<!--sidebar start-->
+<aside>
+    <div id="sidebar" class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu">
+            <li class="active">
+                <a class="" href="#">
+                    <i class="icon_house_alt"></i>
+                    <span>Main</span>
+                </a>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;" class="">
+                    <i class="icon_document_alt"></i>
+                    <span>Licitaciones</span>
+                    <span class="menu-arrow arrow_carrot-right"></span>
+                </a>
+                <ul class="sub">
+
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;" class="">
+                    <i class="icon_chat"></i>
+                    <span>Clientes</span>
+                    <span class="menu-arrow arrow_carrot-right"></span>
+                </a>
+                <ul class="sub">
+
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;" class="">
+                    <i class="icon_desktop"></i>
+                    <span>Productos</span>
+                    <span class="menu-arrow arrow_carrot-right"></span>
+                </a>
+                <ul class="sub">
+
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;" class="">
+                    <i class="icon_desktop"></i>
+                    <span>Stock</span>
+                    <span class="menu-arrow arrow_carrot-right"></span>
+                </a>
+                <ul class="sub">
+
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;" class="">
+                    <i class="icon_calculator_alt"></i>
+                    <span>Compras</span>
+                    <span class="menu-arrow arrow_carrot-right"></span>
+                </a>
+                <ul class="sub">
+                    {{--<li><a class="" href="general.html">Lista</a></li>--}}
+
+                    {{--<li><a class="" href="grids.html">Grids</a></li>--}}
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;" class="">
+                    <i class="icon_profile"></i>
+                    <span>Usuarios</span>
+                    <span class="menu-arrow arrow_carrot-right"></span>
+                </a>
+                <ul class="sub">
+                    {{--<li><a class="" href="general.html">Lista</a></li>--}}
+
+                    {{--<li><a class="" href="grids.html">Grids</a></li>--}}
+                </ul>
+            </li>
+        </ul>
+        <!-- sidebar menu end-->
+    </div>
+</aside>
+<!--sidebar end-->
---
+++ resources/views/template.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/favicon.png">
+    <title>Stock</title>
+    @include('partials.css')
+</head>
+
+<body>
+<!-- container section start -->
+<section id="container" class="">
+
+
+    @include('partials.header')
+    @include('partials.left_navbar')
+
+            <!--main content start-->
+    <section id="main-content">
+        <section class="wrapper">
+
+            @include('partials.breadcrumb')
+
+            @yield('main')
+
+        </section>
+    </section>
+    <!--main content end-->
+</section>
+<!-- container section start -->
+
+@include('partials.javascript')
+
+</body>
+</html>
